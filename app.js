26cbaccc081de08cb7cb01ebf76c56f9052f0d85
qbj700 <qbj700@gmail.com>
2020-12-21 12:58:10 +0900

    2020-12-21, 배포 / 디자인 변경

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function ArticleDetail__Body__init() {
-	if ( toastui === undefined ) {
+	if (toastui === undefined) {
 		return;
 	}
 
@@ -7,11 +7,19 @@ function ArticleDetail__Body__init() {
 	var initialValue = body.innerHTML.trim();
 
 	var viewer = new toastui.Editor.factory({
-		el : body,
-		initialValue : initialValue,
-		viewer : true,
+		el: body,
+		initialValue: initialValue,
+		viewer: true,
 		plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
 	});
 }
 
 ArticleDetail__Body__init();
+
+var topEle = $('.topBtn-2');
+var delay = 500;
+topEle.on('click', function() {
+	$('html, body').stop().animate({
+		scrollTop: 0
+	}, delay);
+});
